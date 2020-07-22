e8e6ee7c4a2d4aabafd6595b8aaaa99756324238
Sacha Schmid <sacha.schmid@yoo.digital>
2020-07-22 14:01:10 +0200

    Updates

---
+++ precache-manifest.7c9f0470ef37521df154e7368e3912b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955fce75c992f769aecbd9fcf82be89a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5725ba4e634ec886167e",
+    "url": "/static/css/main.990aba36.chunk.css"
+  },
+  {
+    "revision": "585054d71d7967bd311e",
+    "url": "/static/js/2.efeed2f2.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.efeed2f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5725ba4e634ec886167e",
+    "url": "/static/js/main.6e1769ab.chunk.js"
+  },
+  {
+    "revision": "2f601c016cb9b2f48532",
+    "url": "/static/js/runtime-main.68ef5f8d.js"
+  }
+]);
