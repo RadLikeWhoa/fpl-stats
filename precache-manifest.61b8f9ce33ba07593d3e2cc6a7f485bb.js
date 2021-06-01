923c8a5620477d079c5917a7260b6c89c9c9eaab
Sacha Schmid <schmid.sacha@gmail.com>
2021-06-01 18:41:21 +0200

    Updates

---
+++ precache-manifest.61b8f9ce33ba07593d3e2cc6a7f485bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d3a44fd21e9e2e1edfa7c3d36f3aad",
+    "url": "/fpl-stats/index.html"
+  },
+  {
+    "revision": "75e11bdd9abd5937960c",
+    "url": "/fpl-stats/static/css/main.9aa1d607.chunk.css"
+  },
+  {
+    "revision": "524865a707da9e4ec180",
+    "url": "/fpl-stats/static/js/2.ce40b103.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/fpl-stats/static/js/2.ce40b103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75e11bdd9abd5937960c",
+    "url": "/fpl-stats/static/js/main.7694b8f0.chunk.js"
+  },
+  {
+    "revision": "1e27a39fc36e13a5dbf7",
+    "url": "/fpl-stats/static/js/runtime-main.a8f838e4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fpl-stats/precache-manifest.2e8c0f56d368abc775b34a73465a7ba8.js"
+  "/fpl-stats/precache-manifest.61b8f9ce33ba07593d3e2cc6a7f485bb.js"
 );
 
 self.addEventListener('message', (event) => {
